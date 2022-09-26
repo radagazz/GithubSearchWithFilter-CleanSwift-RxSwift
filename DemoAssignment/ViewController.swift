e37cda4203de996efba2715b25839d98fe2f038e
Anurak Teerarattananukulchai <45746492+radagazz@users.noreply.github.com>
2022-09-26 23:33:06 +0700

    Initial Commit

---
+++ DemoAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoAssignment
+//
+//  Created by anurak teerarattananukulchai on 26/9/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
